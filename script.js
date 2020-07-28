c7d54546a638ab92ffca3b9221aaa238c5d4c0af
Dmitry Hmel <dima.hmelios@gmail.com>
2020-07-28 16:01:28 +0200

    fix: switch singolo checklist

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-import {criteria} from "./criteria-list.js";
-import {render} from './renderView.js';
+import { criteria } from './criteria-list-singolo.js';
+import { render } from './renderView.js';
 
-window.onload = function(){
-    document.querySelector('.start').addEventListener("click", function(e) {
-        e.stopPropagation();
-        if(e.target.parentElement.tagName == "SECTION") e.target.parentElement.style.display = "none";
-    })
+window.onload = function () {
+  document.querySelector('.start').addEventListener('click', function (e) {
+    e.stopPropagation();
+    if (e.target.parentElement.tagName == 'SECTION')
+      e.target.parentElement.style.display = 'none';
+  });
 
-    const header = document.querySelector('header');
-    const offsetTop = header.offsetTop;
+  const header = document.querySelector('header');
+  const offsetTop = header.offsetTop;
 
-    window.onscroll = () => stickyHeader();
+  window.onscroll = () => stickyHeader();
 
-    function stickyHeader() {
-        if (window.pageYOffset > offsetTop + 100) {
-            header.classList.add("sticky");
-          } else {
-            header.classList.remove("sticky");
-          }
+  function stickyHeader() {
+    if (window.pageYOffset > offsetTop + 100) {
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky');
     }
+  }
 
-    render(criteria);
-}
+  render(criteria);
+};
